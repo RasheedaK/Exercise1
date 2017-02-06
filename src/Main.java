252c9d946792f94b7924f64dad910cbade16629d
krashee <krashee@thoughtworks.com>
2017-02-06 12:36:32 +0530

    Add main method

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String args[]) {
+        List<Allergen> allergens = new ArrayList<>();
+        allergens.add(new Allergen("eggs", 1));
+        allergens.add(new Allergen("peanuts", 2));
+        allergens.add(new Allergen("shellfish", 4));
+        allergens.add(new Allergen("strawberries", 8));
+        allergens.add(new Allergen("tomatoes", 16));
+        allergens.add(new Allergen("chocolate", 32));
+        allergens.add(new Allergen("pollen", 64));
+        allergens.add(new Allergen("cats", 128));
+        AllergyScreener allergyScreener=new AllergyScreener(allergens);
+        System.out.println("Enter AllergyScore");
+        Scanner sc=new Scanner(System.in);
+        int allergyScore=sc.nextInt();
+        System.out.println(allergyScreener.isAllergicTo(allergyScore));
+    }
+}
