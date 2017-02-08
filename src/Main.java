64b5ad3d2b660d0eeb8493a341d2f25c3f5c255c
krashee <krashee@thoughtworks.com>
2017-02-08 12:18:34 +0530

    Move the hardcoded list of Allergens from main to Dependency class

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String args[]) {
-        List<Allergen> allergens = new ArrayList<>();
-        allergens.add(new Allergen("eggs", 1));
-        allergens.add(new Allergen("peanuts", 2));
-        allergens.add(new Allergen("shellfish", 4));
-        allergens.add(new Allergen("strawberries", 8));
-        allergens.add(new Allergen("tomatoes", 16));
-        allergens.add(new Allergen("chocolate", 32));
-        allergens.add(new Allergen("pollen", 64));
-        allergens.add(new Allergen("cats", 128));
+        List<Allergen> allergens=Dependency.getAllergensList();
         AllergyScreener allergyScreener = new AllergyScreener(allergens);
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter your Name");
